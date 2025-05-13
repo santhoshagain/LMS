c45438537af43d208d8deeb8371c9a99af24ffc4
santhoshagain <129575523+santhoshagain@users.noreply.github.com>
2025-05-13 16:43:05 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://elliotalderson0096:Santhosh18@lms.kjnnq.mongodb.net/?retryWrites=true&w=majority&appName=LMS', 
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://:lms.kjnnq.mongodb.net/?retryWrites=true&w=majority&appName=LMS', 
   JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
   PORT: process.env.PORT || 5000
 };
